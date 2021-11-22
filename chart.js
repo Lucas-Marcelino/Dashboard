30706a69a14be9b852a8c01bf152655839b2f65c
Lucas Marcelino <37718741+Lucas-Marcelino@users.noreply.github.com>
2021-11-22 20:59:36 -0300

    chart fix

--- chart.js
+++ chart.js
@@ -1,11 +1,13 @@
 const Producao = document.getElementById('Producao').getContext('2d');
 const Erros = document.getElementById('Erros').getContext('2d');
+const Prod = 20;
+const Err = 4;
 var myChart = new Chart(Producao, {
     type: 'bar',
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: '# of Votes',
+            label: 'PRODUÇÃO',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
@@ -34,25 +36,19 @@ var myChart = new Chart(Producao, {
 var myChart = new Chart(Erros, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['','Erro', 'Produzidos'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: '',
+            data: [0, Err, Prod],
             backgroundColor: [
+                'rgba(255, 255, 255, 0.2)',
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
+                'rgba(255, 255, 255, 0.2)',
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
